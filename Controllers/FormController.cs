d426ec1b64a89645bfc8d8763b9791316f2439d2
Martin Hrabovský <hrabosmartan@seznam.cz>
2019-12-09 21:30:25 +0100

    Minor tweaks

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -1,10 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using RaprockPlaylist.Context;
 using RaprockPlaylist.Models;
-using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql;
 using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore.Internal;
